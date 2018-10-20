0f9054c51dc348556a5609aaa775748c7d84ead3
clarkmcc <bmccauley4x@gmail.com>
2018-10-20 11:08:20 +0000

    1

--- getmetadata.sh
+++ getmetadata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Get GCP Compute Metadata for authenticating squid
 
-touch /etc/squid/username
-touch /etc/squid/password
+/usr/bin/touch /etc/squid/username
+/usr/bin/touch /etc/squid/password
 
 curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/susername > /etc/squid/username
 curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/spassword > /etc/squid/password
