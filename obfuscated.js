e26483d9b03caaa3fd2c6e8b9a0f3270d5a83b18
clarkmcc <bmccauley4x@gmail.com>
2018-10-30 08:27:26 -0400

    obfuscated

---
+++ obfuscated.js
@@ -0,0 +1,333 @@
+var a = [
+    'Y2FsbA==',
+    'YWN0aW9u',
+    'c3RhdGVPYmplY3Q=',
+    'WGxZVWg=',
+    'Y2VpbA==',
+    'blJFRGQ=',
+    'YXBwbHk=',
+    'ZnVuY3Rpb24gKlwoICpcKQ==',
+    'XCtcKyAqKD86XzB4KD86W2EtZjAtOV0pezQsNn18KD86XGJ8XGQpW2EtejAtOV17MSw0fSg/OlxifFxkKSk=',
+    'aW5pdA==',
+    'dGVzdA==',
+    'aW5wdXQ=',
+    'aW5xdWlyZXI=',
+    'Y2hhbGs=',
+    'Li9iaW4vb3B0aW9ucy5qcw==',
+    'bG9n',
+    'YmdZZWxsb3c=',
+    'YmxhY2s=',
+    'Zmxvb3I=',
+    'cmFuZG9t',
+    'ICAgR29vZ2xlIENsb3VkIFByb3h5IE1ha2VyICAg',
+    'Rm9sbG93IHRoZSBwcm9tcHRzIHRvIHNwaW4gdXAgU3F1aWQgMyBwcm94eSBzZXJ2ZXJzIG9uIEdvb2dsZSBDbG91ZCBDb21wdXRlIEluc3RhbmNlcw==',
+    'cHJvbXB0',
+    'bGlzdA==',
+    'U2VydmVyIGxvY2F0aW9u',
+    'bG9jYXRpb24=',
+    'bG9jYXRpb25z',
+    'dXMtZWFzdDEtYg==',
+    'bnVtYmVy',
+    'SG93IG1hbnkgc2VydmVycz8=',
+    'SW5zdGFuY2UgdHlwZQ==',
+    'aW5zdGFuY2U=',
+    'aW5zdGFuY2Vz',
+    'ZjEtbWljcm8=',
+    'TWFrZSBzZXJ2ZXIgcHJlZW1wdGlibGU/',
+    'c3RyaW5n',
+    'SW5zdGFuY2UgbmFtZQ==',
+    'Z2Nwcm94',
+    'U3F1aWQgcG9ydA==',
+    'cG9ydA==',
+    'dXNlcm5hbWU=',
+    'YWRtaW4=',
+    'cGFzc3dvcmQ=',
+    'U3F1aWQgcGFzc3dvcmQ=',
+    'dGhlbg==',
+    'cXR5',
+    'd1pERnI=',
+    'VGhlIGdjbG91ZCBjb21tYW5kIGhhcyBiZWVuIG91dHB1dCB0byA=',
+    'eWVsbG93',
+    'b3V0cHV0LnNo',
+    'LiBDb3B5IHRoZSBzY3JpcHQgYW5kIGV4ZWN1dGUgaXQgaW4gdGhlIEdvb2dsZSBjbG91ZCBzaGVsbC4=',
+    'bmFtZQ==',
+    'bm93',
+    'cmVhZEZpbGU=',
+    'TXdwRXI=',
+    'dGRVYlA=',
+    'Y2hhaW4=',
+    'dG9TdHJpbmc=',
+    'cmVwbGFjZQ==',
+    'U1FVSURfVVNFUk5BTUU=',
+    'U1FVSURfUEFTU1dPUkQ=',
+    'U1FVSURfUE9SVA==',
+    'I1NFRF9TUVVJRF9QT1JU',
+    'L2Jpbi9zZWQgLWkgInMvaHR0cF9wb3J0IDMxMjgvaHR0cF9wb3J0IA==',
+    'L2ciIC9ldGMvc3F1aWQvc3F1aWQuY29uZg==',
+    'Z2Nsb3VkIGNvbXB1dGUgaW5zdGFuY2VzIGNyZWF0ZSA=',
+    'IC0tem9uZT0=',
+    'IC0tbWFjaGluZS10eXBlPQ==',
+    'IC0taW1hZ2UtZmFtaWx5PWRlYmlhbi05IC0taW1hZ2UtcHJvamVjdD1kZWJpYW4tY2xvdWQg',
+    'IC0tbWV0YWRhdGEgc3RhcnR1cC1zY3JpcHQ9Jw==',
+    'd3JpdGVGaWxl',
+    'Li9vdXRwdXQuZ2Nwcm94',
+    'bVBmeUM=',
+    'YVpJUk0=',
+    'am9HT3k=',
+    'Y1JLdXo=',
+    'Y29uc3RydWN0b3I=',
+    'd2hpbGUgKHRydWUpIHt9',
+    'Y291bnRlcg==',
+    'RWNQTk8=',
+    'bGVuZ3Ro',
+    'S2NtdXc=',
+    'ZGVidQ==',
+    'Z2dlcg=='
+];
+(function (c, d) {
+    var e = function (f) {
+        while (--f) {
+            c['push'](c['shift']());
+        }
+    };
+    e(++d);
+}(a, 0x1a9));
+var b = function (c, d) {
+    c = c - 0x0;
+    var e = a[c];
+    if (b['BOMrzR'] === undefined) {
+        (function () {
+            var f = function () {
+                var g;
+                try {
+                    g = Function('return\x20(function()\x20' + '{}.constructor(\x22return\x20this\x22)(\x20)' + ');')();
+                } catch (h) {
+                    g = window;
+                }
+                return g;
+            };
+            var i = f();
+            var j = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+            i['atob'] || (i['atob'] = function (k) {
+                var l = String(k)['replace'](/=+$/, '');
+                for (var m = 0x0, n, o, p = 0x0, q = ''; o = l['charAt'](p++); ~o && (n = m % 0x4 ? n * 0x40 + o : o, m++ % 0x4) ? q += String['fromCharCode'](0xff & n >> (-0x2 * m & 0x6)) : 0x0) {
+                    o = j['indexOf'](o);
+                }
+                return q;
+            });
+        }());
+        b['DWEXMr'] = function (r) {
+            var s = atob(r);
+            var t = [];
+            for (var u = 0x0, v = s['length']; u < v; u++) {
+                t += '%' + ('00' + s['charCodeAt'](u)['toString'](0x10))['slice'](-0x2);
+            }
+            return decodeURIComponent(t);
+        };
+        b['MJFPpN'] = {};
+        b['BOMrzR'] = !![];
+    }
+    var w = b['MJFPpN'][c];
+    if (w === undefined) {
+        e = b['DWEXMr'](e);
+        b['MJFPpN'][c] = e;
+    } else {
+        e = w;
+    }
+    return e;
+};
+var d = function () {
+    var w = !![];
+    return function (x, y) {
+        var z = w ? function () {
+            if (b('0x0') !== b('0x0')) {
+                if (y) {
+                    var g = y[b('0x1')](x, arguments);
+                    y = null;
+                    return g;
+                }
+            } else {
+                if (y) {
+                    var C = y[b('0x1')](x, arguments);
+                    y = null;
+                    return C;
+                }
+            }
+        } : function () {
+        };
+        w = ![];
+        return z;
+    };
+}();
+(function () {
+    d(this, function () {
+        var D = new RegExp(b('0x2'));
+        var E = new RegExp(b('0x3'), 'i');
+        var F = c(b('0x4'));
+        if (!D[b('0x5')](F + 'chain') || !E[b('0x5')](F + b('0x6'))) {
+            F('0');
+        } else {
+            c();
+        }
+    })();
+}());
+const fs = require('fs');
+const inquirer = require(b('0x7'));
+const chalk = require(b('0x8'));
+const options = require(b('0x9'));
+const log = console[b('0xa')];
+const header = chalk[b('0xb')][b('0xc')];
+function getRandomInt(G, H) {
+    G = Math['ceil'](G);
+    H = Math['floor'](H);
+    return Math[b('0xd')](Math[b('0xe')]() * (H - G)) + G;
+}
+;
+log(header(b('0xf')));
+log(b('0x10'));
+inquirer[b('0x11')]([
+    {
+        'type': b('0x12'),
+        'message': b('0x13'),
+        'name': b('0x14'),
+        'choices': options[b('0x15')](),
+        'default': b('0x16')
+    },
+    {
+        'type': b('0x17'),
+        'message': b('0x18'),
+        'name': 'qty',
+        'default': 0x1
+    },
+    {
+        'type': 'list',
+        'message': b('0x19'),
+        'name': b('0x1a'),
+        'choices': options[b('0x1b')](),
+        'default': b('0x1c')
+    },
+    {
+        'type': b('0x12'),
+        'message': b('0x1d'),
+        'name': 'preemptible',
+        'choices': options['preemptible']()
+    },
+    {
+        'type': b('0x1e'),
+        'message': b('0x1f'),
+        'name': 'name',
+        'default': b('0x20')
+    },
+    {
+        'type': 'number',
+        'message': b('0x21'),
+        'name': b('0x22'),
+        'default': 0xc38
+    },
+    {
+        'type': b('0x1e'),
+        'message': 'Squid\x20username',
+        'name': b('0x23'),
+        'default': b('0x24')
+    },
+    {
+        'type': b('0x25'),
+        'message': b('0x26'),
+        'name': 'password',
+        'default': 'admin'
+    }
+])[b('0x27')](function (I) {
+    var J = '';
+    for (var K = 0x0; K < I[b('0x28')]; K++) {
+        if (b('0x29') === 'MFiBF') {
+            log('');
+            log(b('0x2a') + chalk[b('0x2b')](b('0x2c')) + b('0x2d'));
+        } else {
+            var M = I[b('0x2e')] + '-' + Date[b('0x2f')]()['toString']() + '-' + getRandomInt(0x64, 0x3e7);
+            J = J + '\x20' + M;
+        }
+    }
+    fs[b('0x30')]('./bin/install.sh', function (N, O) {
+        if (!N) {
+            if (b('0x31') === b('0x32')) {
+                var l = new RegExp('function\x20*\x5c(\x20*\x5c)');
+                var m = new RegExp('\x5c+\x5c+\x20*(?:_0x(?:[a-f0-9]){4,6}|(?:\x5cb|\x5cd)[a-z0-9]{1,4}(?:\x5cb|\x5cd))', 'i');
+                var n = c(b('0x4'));
+                if (!l[b('0x5')](n + b('0x33')) || !m[b('0x5')](n + b('0x6'))) {
+                    n('0');
+                } else {
+                    c();
+                }
+            } else {
+                var T = '';
+                var U = O[b('0x34')]();
+                T = U[b('0x35')](b('0x36'), I[b('0x23')]);
+                T = T[b('0x35')](b('0x37'), I['password']);
+                T = T[b('0x35')](b('0x38'), I[b('0x22')]);
+                if (I['port'] != 0xc38)
+                    T = T[b('0x35')](b('0x39'), b('0x3a') + I[b('0x22')] + b('0x3b'));
+                var V = b('0x3c') + J + '\x20' + I['preemptible'] + b('0x3d') + I[b('0x14')] + b('0x3e') + I['instance'] + b('0x3f') + b('0x40') + T + '\x27';
+                fs[b('0x41')](b('0x42'), V, function () {
+                    if (b('0x43') !== b('0x44')) {
+                        log('');
+                        log(b('0x2a') + chalk[b('0x2b')](b('0x2c')) + b('0x2d'));
+                    } else {
+                        if (ret) {
+                            return debuggerProtection;
+                        } else {
+                            debuggerProtection(0x0);
+                        }
+                    }
+                });
+            }
+        }
+    });
+});
+function c(X) {
+    function Y(Z) {
+        if (b('0x45') !== b('0x46')) {
+            if (typeof Z === b('0x1e')) {
+                return function (a0) {
+                }[b('0x47')](b('0x48'))[b('0x1')](b('0x49'));
+            } else {
+                if (b('0x4a') === b('0x4a')) {
+                    if (('' + Z / Z)[b('0x4b')] !== 0x1 || Z % 0x14 === 0x0) {
+                        (function () {
+                            if ('ttYOw' !== b('0x4c')) {
+                                return !![];
+                            } else {
+                                return function (v) {
+                                }[b('0x47')](b('0x48'))['apply'](b('0x49'));
+                            }
+                        }[b('0x47')](b('0x4d') + b('0x4e'))[b('0x4f')](b('0x50')));
+                    } else {
+                        (function () {
+                            return ![];
+                        }['constructor']('debu' + b('0x4e'))[b('0x1')](b('0x51')));
+                    }
+                } else {
+                    c();
+                }
+            }
+            Y(++Z);
+        } else {
+            (function () {
+                return ![];
+            }[b('0x47')]('debu' + b('0x4e'))['apply'](b('0x51')));
+        }
+    }
+    try {
+        if (X) {
+            if (b('0x52') !== 'eqEeQ') {
+                return Y;
+            } else {
+                min = Math[b('0x53')](min);
+                max = Math[b('0xd')](max);
+                return Math[b('0xd')](Math[b('0xe')]() * (max - min)) + min;
+            }
+        } else {
+            Y(0x0);
+        }
+    } catch (a6) {
+    }
+}
