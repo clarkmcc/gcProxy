6a699dfa7215cca9ef8af9eca9ab86f08a3b8dfd
clarkmcc <bmccauley4x@gmail.com>
2018-11-13 13:15:59 +0000

    Added support for express serving public dir

--- auth/start.js
+++ auth/start.js
@@ -1,15 +1,22 @@
 // Documentation - https://cloud.google.com/nodejs/docs/reference/compute/0.10.x/VM
-'use strict';
+"use strict";
+const express = require("express");
+const app = express()
 const fsutil = require("./bin/fs-utilities.js");
 const chalk = require("chalk")
 const cliui = require("cliui")();
 const vm = require("./bin/vm.js");
 const ui = require("./bin/ui.js")
-const Compute = require('@google-cloud/compute');
+const Compute = require("@google-cloud/compute");
 const compute = new Compute({
-  projectId: 'ppmproxy',
-  keyFilename: './keyfile.json'
+  projectId: "ppmproxy",
+  keyFilename: "./bin/keyfile.json"
 });
 
+app.use(express.static("public"))
 
-ui.getVMs(compute, vm, cliui)
+// ui.getVMs(compute, vm, cliui)
+
+app.listen(process.env.PORT || 3000, function(err) {
+    if(err) console.error
+})
